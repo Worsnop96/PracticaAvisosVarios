d09d7dae9749ad7bdec429fca4fd01ee248dea5f
Dylan Estrada <dylan@hotmail.com>
2021-10-22 16:05:49 -0500

    Ejercicio de Avisos Varios

---
+++ AvisosTrafico.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvisosVarios
+{
+    class AvisosTrafico : IAvisos
+    {
+
+        public AvisosTrafico()
+        {
+            remitente = "DGT";
+            mensaje = "Sancion comedita. Si paga en el mes correspondiente puede ahorrarse un 50% en sus multas";
+            fecha = "";
+        }
+
+        public AvisosTrafico(string remitente, string mensaje, string fecha)
+        {
+            this.remitente= remitente;
+            this.mensaje = mensaje;
+            this.fecha = fecha;
+
+        }
+
+
+        public string getFecha()
+        {
+            return fecha;
+        }
+
+        public void mostrarAviso()
+        {
+            Console.WriteLine("Mensaje {0} enviado por {1} el dia {2}", mensaje, remitente, fecha);
+        }
+
+        private string remitente;
+        private string mensaje;
+        private string fecha;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AvisosVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AvisosTrafico general = new AvisosTrafico();
+            general.mostrarAviso();
+
+            AvisosTrafico carlosMadrid = new AvisosTrafico("Direccion General de Madrid", "Multa por exceso de velodicdad", "18/08/2020");
+            carlosMadrid.mostrarAviso();
+
+        }
+    }
+}
